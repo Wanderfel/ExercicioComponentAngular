35281b57aa04efc094698bb75d94b9513b06f8d6
Wanderfel <wanderfel231@gmail.com>
2021-08-17 20:11:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-mja7vy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-mja7vy)
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatButtonModule,
+    MatMenuModule
+  ],
+  declarations: [MenuComponent],
+  exports: [MenuComponent]
+})
+export class LayoutModule {}
